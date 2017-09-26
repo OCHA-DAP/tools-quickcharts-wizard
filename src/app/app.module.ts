9b401660a4b530dae8a398cd09b48d8774494d80
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2017-09-27 02:14:16 +0300

    HDX-5545 initial Google Picker supprt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ToolsWizardComponent } from './tools-wizard/tools-wizard.component';
@@ -18,7 +19,8 @@ import { ShareComponent } from './tools-wizard/share/share.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tools-wizard/import/import.component.ts
+++ src/app/tools-wizard/import/import.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+
+declare const gapi: any;
+declare const google: any;
 
 @Component({
   selector: 'app-import',
@@ -7,9 +10,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportComponent implements OnInit {
 
-  constructor() { }
+  // The Browser API key obtained from the Google API Console.
+  readonly developerKey = 'AIzaSyAFWbZlgf6DUdmLxrypnZs1K7_VJJenqH0';
+
+  // The Client ID obtained from the Google API Console. Replace with your own Client ID.
+  readonly clientId = '838107907608-ki8i6mus7o19ohv8oigutjej5om1p90h.apps.googleusercontent.com';
+
+  // Scope to use to access user's photos.
+  readonly scope = ['https://www.googleapis.com/auth/drive.readonly'];
+
+  pickerApiLoaded = false;
+  oauthToken;
+
+  selectedUrl = '';
+
+  constructor(private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
   }
+
+  loadGooglePicker() {
+    let picker = null;
+    const onAuthApiLoad = function () {
+      gapi.auth.authorize(
+          {
+            'client_id': this.clientId,
+            'scope': this.scope,
+            'immediate': false
+          },
+          handleAuthResult);
+    }.bind(this);
+
+    const onPickerApiLoad = function () {
+      this.pickerApiLoaded = true;
+      createPicker();
+    }.bind(this);
+
+    const handleAuthResult = function (authResult) {
+      if (authResult && !authResult.error) {
+        this.oauthToken = authResult.access_token;
+        createPicker();
+      }
+    }.bind(this);
+
+    const createPicker = function () {
+      if (this.pickerApiLoaded && this.oauthToken) {
+        picker = new google.picker.PickerBuilder().
+            addView(google.picker.ViewId.DOCS).
+            setOAuthToken(this.oauthToken).
+            setDeveloperKey(this.developerKey).
+            setCallback(pickerCallback).
+            build();
+        picker.setVisible(true);
+      }
+    }.bind(this);
+
+    const pickerCallback = function (data) {
+      let url = 'nothing';
+      if (data[google.picker.Response.ACTION] === google.picker.Action.PICKED) {
+        const doc = data[google.picker.Response.DOCUMENTS][0];
+        url = doc[google.picker.Document.URL];
+      }
+      const message = 'You picked: ' + url;
+      console.log(message);
+      this.selectedUrl = url;
+      picker.setVisible(false);
+      this.cd.detectChanges();
+    }.bind(this);
+
+    gapi.load('auth', {'callback': onAuthApiLoad});
+    gapi.load('picker', {'callback': onPickerApiLoad});
+  }
+
 
 }
