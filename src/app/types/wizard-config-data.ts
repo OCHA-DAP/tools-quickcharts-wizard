6df31a90c0e14f2f05e81be6736ada4858073422
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2017-11-01 16:39:26 +0200

    Changing default recipe

--- src/app/types/wizard-config-data.ts
+++ src/app/types/wizard-config-data.ts
@@ -2,6 +2,6 @@ export class WizardConfigData {
     step1Sample = true;
     step2Sample = true;
     url: string;
-    recipeUrl = 'https://raw.githubusercontent.com/OCHA-DAP/hdx-hxl-preview/master/src/assets/bites.json';
+    recipeUrl = 'https://raw.githubusercontent.com/OCHA-DAP/hxl-recipes/master/recipes/hdx/recipe.json';
     hxlCheckError = null;
 }
