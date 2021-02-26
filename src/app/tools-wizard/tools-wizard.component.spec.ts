86075bedad90978f48a384e9662aea513a22b48a
Alexandru Artimon <aalecs@gmail.com>
2021-02-26 17:30:38 +0200

    upgrade to angular 11

--- src/app/tools-wizard/tools-wizard.component.spec.ts
+++ src/app/tools-wizard/tools-wizard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ToolsWizardComponent } from './tools-wizard.component';
 
@@ -6,7 +6,7 @@ describe('ToolsWizardComponent', () => {
   let component: ToolsWizardComponent;
   let fixture: ComponentFixture<ToolsWizardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ToolsWizardComponent ]
     })
